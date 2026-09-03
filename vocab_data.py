84fd78c3ce666384b6600cec972b36c11eeeaf50
koos8mc-design <koos8mc@gmail.com>
2026-09-03 21:05:00 +0200

    Create vocab_data.py

---
+++ vocab_data.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Woordenlijsten Grieks - Nederlands
+Elke lijst is een lijst van tuples: (Grieks, Nederlands)
+"""
+
+TEKST_8A = [
+    ("πάρειμι", "aanwezig zijn"),
+    ("εἶπον (imperf. van λέγω)", "ik zei"),
+    ("οἱ Ἀχαιοί", "Grieken"),
+    ("βοηθέω (+ dat.)", "te hulp komen, helpen"),
+    ("ὁ ζευγηλάτης", "legeraanvoerder"),
+    ("ὁ ναύτης", "zeeman, matroos"),
+    ("ὁ ὁπλίτης", "soldaat"),
+    ("παύω", "(doen/laten) stoppen, ophouden"),
+    ("κωλύω (+ inf.)", "verhinderen, beletten (om)"),
+    ("ἅμα", "tegelijk, tegelijkertijd"),
+    ("τόδε (onz. mv. τάδε)", "dit / het volgende"),
+    ("οὐδέ", "en niet, maar niet / ook niet, zelfs niet"),
+    ("ἐχθρός (+ dat.)", "gehaat (bij), vijandig (aan)"),
+]
+
+TEKST_8B = [
+    ("ἡ τύχη", "lot"),
+    ("τοι", "toch"),
+    ("ὁ ἄγγελος", "bode"),
+    ("ἡ ἀλήθεια", "waarheid"),
+    ("ἀγγέλλω", "berichten, melden"),
+    ("τὸ στράτευμα", "leger"),
+    ("πιστεύω (+ dat.)", "vertrouwen"),
+    ("τοιοῦτος, τοιαύτη, τοιοῦτο(ν)", "zodanig, dergelijk, zo'n"),
+    ("φίλτατος, φιλτάτη", "liefst, dierbaarst / zeer geliefd, zeer dierbaar"),
+    ("ὁμολογέω (+ dat.)", "instemmen met, akkoord gaan met"),
+    ("ἱκετεύω", "smeken"),
+]
+
+TEKST_8C = [
+    ("ὁ βωμός", "altaar"),
+    ("οὐδέν", "niets"),
